6c85caf532cc432e3b36233f5e2e77e9807a34a1
jhon anderson <apxanderson02@gmail.com>
2022-03-12 22:32:16 -0500

    estructura snake

---
+++ js/index.js
@@ -0,0 +1,51 @@
+window.addEventListener("load",()=>{
+    const tablero = document.querySelector(".tablero");
+
+    const board = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+
+    class Snake {
+        constructor(posiciones, tamaño){
+            this.posiciones = posiciones;
+            this.tamaño = tamaño;
+            this.score = 0;
+        }
+    }
+
+    const dibujar = (x,y)=>{
+        tablero.innerHTML ="";
+        for(let i = 0; i< 10; i++) {
+            for(let j = 0; j < 10; j++) {
+                if(i===x && j===y){
+                    tablero.innerHTML += `<div class="food"></div>`
+
+                }else{
+                tablero.innerHTML += `<div class="casilla"></div>`
+
+                }
+            }
+            
+        }
+    }
+    let x,y;
+    setInterval(()=>{
+       x = Math.floor(Math.random() * 10)
+       y = Math.floor(Math.random() * 10)
+        console.log(x,y)
+        dibujar(x,y)
+      
+        
+
+    },1000)
+
+})
